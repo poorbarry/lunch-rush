f2a6c273ad042dd6c1b24e2ec76aa04e306c29d1
Dmusz <dan.muszynski@cengage.com>
2018-11-28 09:52:42 -0500

    progress from Nov 2

--- src/CurrentUser.js
+++ src/CurrentUser.js
@@ -1,10 +1,15 @@
-import React, { PropTypes } from 'react';
-import { auth } from './firebase';
-import './CurrentUser.css';
+import React, { PropTypes } from "react";
+import { auth } from "./firebase";
+import "./CurrentUser.css";
 
 const CurrentUser = ({ user }) => {
   return (
     <div className="CurrentUser">
+      <img
+        className="CurrentUser--photo"
+        src={user.photoURL}
+        alt={user.displayName}
+      />
     </div>
   );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: "AIzaSyB3gokZv6AWzZP45kI-iAQ6EqXXFf5z-eg",
+  authDomain: "lunch-rush-58bc8.firebaseapp.com",
+  databaseURL: "https://lunch-rush-58bc8.firebaseio.com",
+  projectId: "lunch-rush-58bc8",
+  storageBucket: "lunch-rush-58bc8.appspot.com",
+  messagingSenderId: "928863949759"
 };
 
 firebase.initializeApp(config);
